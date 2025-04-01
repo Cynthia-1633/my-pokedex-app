3922d7c32b4c5f8bf8cf9dc7baf5df4f42449eb3
Morelle <cynthia.morelle.cm@gmail.com>
2025-04-01 17:13:43 +0200

    ajout d'une prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
 import "./App.css";
 import PokemonCard from "./components/PokemonCard.tsx";
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function App() {
   return (
     <div>
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -9,22 +9,15 @@ const pokemonList = [
   },
 ];
 
-function PokemonCard() {
+function PokemonCard(props) {
+  console.log(props);
+
+  // ...
+}
+
+/*function PokemonCard() {
   const pokemon = pokemonList[0];
-  /*return (
-    <article className="pokemonCard">
-      <h1>{pokemon.name}</h1>
-      <img src={pokemon.imgSrc} alt={pokemon.name} />
-    </article>
-  );*/
-
-  /*if (pokemon.imgSrc) {
-    return <img src={pokemon.imgSrc}/>;
-    }
-  else {
-    return <p>{"???"}</p>; 
-}*/
-
+ 
   return (
     <figure>
       {pokemon.imgSrc ? (
@@ -36,9 +29,6 @@ function PokemonCard() {
     </figure>
   );
 }
-
-/*Modifie ton code pour afficher l'image du Pokémon seulement si pokemon.imgSrc est défini. 
-Si l'image est disponible, utilise une balise <img> pour l'afficher. 
-Sinon, affiche un paragraphe <p> avec "???" en texte. Utilise l'opérateur ternaire ?: pour cela.*/
+*/
 
 export default PokemonCard;
